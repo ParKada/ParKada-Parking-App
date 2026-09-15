f218bd43d41b7437d14c9f159d29c8dc04520ec2
rivioops <reginangeli.cadelina@gmail.com>
2026-09-15 21:22:55 +0800

    fix(mobile): patch react-native-css-interop to prevent NativeWind stringify crash

--- apps/mobile/components/SafeModal.tsx
+++ apps/mobile/components/SafeModal.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import { View, ModalProps, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Modal as RNModal, ModalProps } from 'react-native';
 
 export function Modal(props: ModalProps) {
   if (!props.visible) return null;
-  
-  return (
-    <View style={StyleSheet.absoluteFill} style={[{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 9999, elevation: 9999 }]}>
-      {props.children}
-    </View>
-  );
+  return <RNModal {...props} visible={true} />;
 }
